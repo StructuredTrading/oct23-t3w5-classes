86abfb5f9e6a3d4a0c998f3107499458bb40f960
StructuredTrading <samowen1992@hotmail.com>
2024-05-04 09:49:23 +1000

    feat: class basic functionality and inheritance

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 // Import Library class, Book class, Audio class, Games class, Movies class, Music class
 
+const { Book } = require("./Book");
+const { Library } = require("./Library");
 const { Media } = require("./Media");
 
 // Create instances of classes
 
-let lotr = new Media("fellowship of the ring");
+// Lord of the Rings as a instance of Book class
+let lotr = new Book("fellowship of the ring", "fancy");
 
 console.log(lotr);
+console.log(lotr.name);
+console.log(lotr.edition);
 
-// Lord of the Rings as a instance of Book class
 
 
 // Create a instance of a Library
 // add media instances into the Library instance
 
+let libraryInstance = new Library();
+libraryInstance.books.push(lotr);
+
 
 // Log the data in the library
+console.log(libraryInstance);
+
+console.log(libraryInstance.books[0].name);
