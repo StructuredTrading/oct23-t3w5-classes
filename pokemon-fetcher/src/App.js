1c5d0e8b83239111b9b7ef810da6a6326230a9e6
StructuredTrading <samowen1992@hotmail.com>
2024-05-04 10:54:31 +1000

    feat: pokemon card component rendering

---
+++ pokemon-fetcher/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import PokemonTeam from './components/PokemonTeam';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Pokemon Team Generator</h1>
+      <PokemonTeam />
+    </div>
+  );
+}
+
+export default App;
